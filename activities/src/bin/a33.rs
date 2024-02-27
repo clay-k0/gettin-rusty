193549f82c730b39c2d0d0d589fe3fbe7feff6bf
Clayton King <ClaytonKing03@gmail.com>
2024-02-27 01:12:23 -0500

    finished a32 & a33

--- activities/src/bin/a33.rs
+++ activities/src/bin/a33.rs
@@ -8,8 +8,16 @@
 // * No data may be copied (cannot use .to_owned() or .to_string())
 // * If both strings are the same length, the first one should be returned
 
+fn longest<'a>(word_one: &'a str, word_two: &'a str) -> &'a str {
+    if word_two.len() > word_one.len() {
+        word_two
+    } else {
+        word_one
+    }
+}
+
 fn main() {
     let short = "hello";
-    let long = "this is a long message";
-    println!("{}", longest(short, long))
+    let long = "this is longer";
+    println!("{}", longest(short, long));
 }
