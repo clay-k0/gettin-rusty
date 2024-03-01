cde773e6e414c1a3271cab6b758dfda9000471e2
Clayton King <ClaytonKing03@gmail.com>
2024-03-01 01:47:57 -0500

    added typestate demo and finished typestate activity (a34)

--- activities/src/bin/a34.rs
+++ activities/src/bin/a34.rs
@@ -15,5 +15,75 @@
 //   * EndCustody      (luggage was picked up by passenger)
 // Notes:
 // * Optionally use generics for each state
+use chrono::Utc;
 
-fn main() {}
+#[derive(Debug, Clone, Copy)]
+struct LuggageID(usize);
+
+struct Luggage(LuggageID);
+
+struct CheckIn(LuggageID);
+struct OnLoading(LuggageID);
+struct OffLoading(LuggageID);
+struct AwaitingPickup(LuggageID);
+
+struct EndCustody(LuggageID);
+
+impl Luggage {
+    fn new(id: LuggageID) -> Self {
+        println!("\n{:?}", id);
+        println!("-------------------");
+
+        Luggage(id)
+    }
+
+    fn check_in(self) -> CheckIn {
+        println!("> Checking In at {}", Utc::now());
+        CheckIn(self.0)
+    }
+}
+
+impl CheckIn {
+    fn on_load(self) -> OnLoading {
+        println!("> Loading Onto Plane");
+        OnLoading(self.0)
+    }
+}
+
+impl OnLoading {
+    fn off_load(self) -> OffLoading {
+        println!("> Unloading From Plane");
+        OffLoading(self.0)
+    }
+}
+
+impl OffLoading {
+    fn await_pickup(self) -> AwaitingPickup {
+        println!("> Awaiting Pickup");
+        AwaitingPickup(self.0)
+    }
+}
+
+impl AwaitingPickup {
+    fn pickup(self) -> (Luggage, EndCustody) {
+        println!("> Getting Picked Up By Passenger at {}", Utc::now());
+        (Luggage(self.0), EndCustody(self.0))
+    }
+}
+
+fn main() {
+    let id1 = LuggageID(49538);
+    let luggage1 = Luggage::new(id1);
+
+    luggage1
+        .check_in()
+        .on_load()
+        .off_load()
+        .await_pickup()
+        .pickup();
+
+    let id2 = LuggageID(49538);
+    let luggage2 = Luggage::new(id2);
+
+    luggage2.check_in().on_load().off_load().await_pickup();
+}
---
+++ activities/src/bin/demo-typestate-pattern.rs
@@ -0,0 +1,66 @@
+struct Employee<State> {
+    name: String,
+    state: State,
+}
+
+impl<State> Employee<State> {
+    fn transition<NextState>(self, state: NextState) -> Employee<NextState> {
+        Employee {
+            name: self.name,
+            state,
+        }
+    }
+}
+
+struct Agreement;
+struct Signature;
+struct Training;
+struct FailedTraining {
+    score: u8,
+}
+struct OnboardingComplete {
+    score: u8,
+}
+
+impl Employee<Agreement> {
+    fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            state: Agreement,
+        }
+    }
+    fn read_agreement(self) -> Employee<Signature> {
+        self.transition(Signature)
+    }
+}
+
+impl Employee<Signature> {
+    fn sign(self) -> Employee<Training> {
+        self.transition(Training)
+    }
+}
+
+impl Employee<Training> {
+    fn train(self, score: u8) -> Result<Employee<OnboardingComplete>, Employee<FailedTraining>> {
+        if score >= 70 {
+            Ok(self.transition(OnboardingComplete { score }))
+        } else {
+            Err(self.transition(FailedTraining { score }))
+        }
+    }
+}
+
+fn main() {
+    let employee_one = Employee::new("Clay");
+    let onboarding_process = employee_one.read_agreement().sign().train(90);
+    match onboarding_process {
+        Ok(emp) => println!(
+            "{}'s Onbarding is Complete. Score: {}/100",
+            emp.name, emp.state.score
+        ),
+        Err(emp) => println!(
+            "{}'s Training Failed. Score: {}/100",
+            emp.name, emp.state.score
+        ),
+    }
+}
