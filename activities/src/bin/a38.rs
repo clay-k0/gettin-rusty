a50447d0b7244ee4d145da4d621beee33ccd457e
Clayton King <ClaytonKing03@gmail.com>
2024-03-06 23:53:58 -0500

    finished a38

--- activities/src/bin/a38.rs
+++ activities/src/bin/a38.rs
@@ -7,23 +7,32 @@
 //
 // Notes:
 // * Use the join function to wait for threads to finish
+use std::thread::{self, JoinHandle};
+use std::time::Duration;
 
 fn msg_hello() -> &'static str {
-    use std::time::Duration;
-    std::thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1000));
     "Hello, "
 }
 
 fn msg_thread() -> &'static str {
-    use std::time::Duration;
-    std::thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1000));
     "threads"
 }
 
 fn msg_excited() -> &'static str {
-    use std::time::Duration;
-    std::thread::sleep(Duration::from_millis(1000));
+    thread::sleep(Duration::from_millis(1000));
     "!"
 }
 
-fn main() {}
+fn main() {
+    let first_part: JoinHandle<&'static str> = thread::spawn(move || msg_hello());
+    let second_part: JoinHandle<&'static str> = thread::spawn(move || msg_thread());
+    let third_part: JoinHandle<&'static str> = thread::spawn(move || msg_excited());
+
+    let first_result = first_part.join().expect("failed to join at first_result");
+    let second_result = second_part.join().expect("failed to join at second_result");
+    let third_result = third_part.join().expect("failed to join at third_result");
+
+    println!("{}{}{}", first_result, second_result, third_result);
+}
